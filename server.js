3d42362f9978e596d844e69ad40f525fe15bf78e
Otello <otello.hanara@gmail.com>
2016-12-17 18:40:57 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.listen(port, function () {
+    console.log('Express server started on port '+port+'!');
+});
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req, res, next) {
+        console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+        next();
+    }
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+    res.send('About Us');
+})
+
+app.use(express.static(__dirname + '/public'));
