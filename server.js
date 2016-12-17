a0b27bf8bb1f323cb96fbd19bf4843acaf61cd61
Otello <otello.hanara@gmail.com>
2016-12-17 18:52:50 +0200

    Refactor middleware

--- server.js
+++ server.js
@@ -5,20 +5,10 @@ var port = 3000;
 app.listen(port, function () {
     console.log('Express server started on port '+port+'!');
 });
+var middleware = require('./middleware.js')
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private route hit!');
-        next();
-    },
-    logger: function(req, res, next) {
-        console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
-        next();
-    }
-};
 
 app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
     res.send('About Us!');
